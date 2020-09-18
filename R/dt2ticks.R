f2543000838f332739806a075c8f21344ac96ee3
afonsohbm <afonso.hbm@gmail.com>
2020-09-18 18:59:47 -0300

    Arrumando o exemplo da documentação do dt2ticks()

--- R/dt2ticks.R
+++ R/dt2ticks.R
@@ -8,7 +8,7 @@
 #' @return `double()`
 #' @export
 #'
-#' @example `dt2ticks("2020/07/09 11:00:00")` #> [1] "637299000000000000"
+#' @example dt2ticks("2020/07/09 11:00:00") \n # "637299000000000000"
 #'
 dt2ticks <- function(dt){
   format(as.double.POSIXlt(dt) * 1e7 + 621355968000000000, scientific = FALSE)
